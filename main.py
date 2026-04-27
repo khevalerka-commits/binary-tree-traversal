d41daeb8b52b9230d969bc5c513f8ce759ecd43e
khevalerka-commits <khevalerka@gmail.com>
2026-04-27 09:50:46 +0500

    Implemented binary tree traversal in main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = TreeNode(root)
+
+    def preorder(self, node):
+        if node:
+            print(node.value, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.value, end=' ')
+            self.inorder(node.right)
+
+    def postorder(self, node):
+        if node:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.value, end=' ')
+
+if __name__ == '__main__':
+    # Sample tree:
+    #        1
+    #       / \
+    #      2   3
+    #     / \
+    #    4   5
+    tree = BinaryTree(1)
+    tree.root.left = TreeNode(2)
+    tree.root.right = TreeNode(3)
+    tree.root.left.left = TreeNode(4)
+    tree.root.left.right = TreeNode(5)
+
+    print('Preorder traversal:')
+    tree.preorder(tree.root)
+    print('\nInorder traversal:')
+    tree.inorder(tree.root)
+    print('\nPostorder traversal:')
+    tree.postorder(tree.root)
